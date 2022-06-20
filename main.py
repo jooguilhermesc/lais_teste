4761f5e851cbf3e9f9388407338275ffdf2e57a9
João Guilherme Silva Cabral <46795337+jooguilhermesc@users.noreply.github.com>
2022-06-20 11:30:22 -0300

    adiciona condicional para outros nomes
    O código foi adaptado para enviar mensagens personalizadas para outros usuários, além disso, foi inserida uma função que calcula a média entre dois números utilizando o git copilot

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ nome = os.getenv("NOME")
 
 if nome == 'Laís':
     print('Olá, Laís!')
+elif nome == 'João':
+    print('Olá, João! Tudo bem?')
 else:
     print('Olá, visitante!')
     
+    
+"""quero uma função que calcula a média entre dois números"""
+
+def media(n1, n2):
+    return (n1 + n2) / 2
+
+a = media(3,2)
+print(a)
